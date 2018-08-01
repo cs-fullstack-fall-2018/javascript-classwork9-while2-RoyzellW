92cb6d119179bbbd42878aee6dcb7f72f2641ba0
student <royzellwalton@gmail.com>
2018-07-31 21:21:02 -0500

    Done!

--- Min.js
+++ Min.js
@@ -1,6 +1,14 @@
 
-var userInput = prompt("Enter a name or q to quit: ");
-while (userInput != "q") {
-    var userInput = prompt("Enter q to quit: ");
-}
+// var userInput = prompt("Enter a name or q to quit: ");
+// while (userInput != "q") {
+//     var userInput = prompt("Enter q to quit: ");
+// }
 
+var password = "Cow";
+var userInput = prompt("Enter password: ");
+while (userInput != password) {
+    var userInput = prompt("Please enter password: ");
+    if (userInput == password) {
+        alert("That's correct!")
+    }
+}
